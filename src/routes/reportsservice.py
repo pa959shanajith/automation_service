ae2e9e9bd6997eb59fb343a77a05b6f5dfb4e03b
Priyanka R <priyanka.r@slkgroup.com>
2019-11-05 17:01:33 +0530

    #1948-Reporting Services-NDAC/NodeJS API's Rewrite

---
+++ src/routes/reportsservice.py
@@ -0,0 +1,157 @@
+################################################################################
+# BEGIN OF DEFAULT METHODS AND IMPORTS       -----------DO NOT EDIT
+################################################################################
+#----------DEFAULT METHODS AND IMPORTS------------DO NOT EDIT-------------------
+from utils import *
+
+def LoadServices(app, redissession, n68session2, webocularsession):
+    setenv(app)
+
+################################################################################
+# END OF DEFAULT METHODS AND IMPORTS       -----------DO NOT EDIT
+################################################################################
+
+
+################################################################################
+# ADD YOUR ROUTES BELOW
+################################################################################
+
+# BEGIN OF REPORTS
+# INCLUDES : all reports related actions
+
+
+#fetching all the suite details
+    @app.route('/reports/getAllSuites_ICE',methods=['POST'])
+    def getAllSuites_ICE():
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            app.logger.debug("Inside getAllSuites_ICE. Query: "+str(requestdata["query"]))
+            if not isemptyrequest(requestdata):
+                if(requestdata["query"] == 'projects'):
+                    queryresult1=n68session2.users.find_one({"_id": ObjectId(requestdata["userid"])},{"projects":1,"_id":0})
+                    queryresult=list(n68session2.projects.find({"_id":{"$in":queryresult1["projects"]}},{"name":1,"releases":1}))
+                    res= {"rows":queryresult}
+                elif(requestdata["query"] == 'getAlltestSuites'):
+                    queryresult=list(n68session2.testsuites.find({"cycle": ObjectId(requestdata["id"])},{"_id":1,"name":1}))
+                    res= {"rows":queryresult}
+            else:
+                app.logger.warn('Empty data received. report suites details.')
+        except Exception as getAllSuitesexc:
+            servicesException("getAllSuites_ICE",getAllSuitesexc)
+        return jsonify(res)
+
+    #fetching all the suite after execution
+    @app.route('/reports/getSuiteDetailsInExecution_ICE',methods=['POST'])
+    def getSuiteDetailsInExecution_ICE():
+        app.logger.debug("Inside getSuiteDetailsInExecution_ICE")
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            if not isemptyrequest(requestdata):
+                queryresult1=n68session2.testsuites.find_one({"_id": ObjectId(requestdata["suiteid"])},{"testscenarioids":1})
+                queryresult=list(n68session2.executions.find({"_id":{"$in":queryresult1["testscenarioids"]}},{"_id":1,"starttime":1,"endtime":1,"status":1}))
+                res= {"rows":queryresult}
+            else:
+                app.logger.warn('Empty data received. report suites details execution.')
+        except Exception as getsuitedetailsexc:
+            servicesException("getSuiteDetailsInExecution_ICE",getsuitedetailsexc)
+        return jsonify(res)
+
+
+    #fetching all the reports status
+    @app.route('/reports/reportStatusScenarios_ICE',methods=['POST'])
+    def reportStatusScenarios_ICE():
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            app.logger.debug("Inside reportStatusScenarios_ICE. Query: "+str(requestdata["query"]))
+            if not isemptyrequest(requestdata):
+                if(requestdata["query"] == 'executiondetails'):
+                    queryresult = list(n68session2.reports.find({"executionid":ObjectId(requestdata["executionid"]),"testsuiteid":ObjectId(requestdata["testsuiteid"])},{"_id":1,"executionid":1,"executedon":1,"comments":1,"executedtime":1,"modifiedby":1,"modifiedbyrole":1,"modifiedon":1,"status":1,"report":1,"testscenarioid":1}))
+                    res= {"rows":queryresult}
+                elif(requestdata["query"] == 'scenarioname'):
+                    queryresult = list(n68session2.testscenarios.find({"_id":ObjectId(requestdata["scenarioid"])},{"name":1}))
+                    res= {"rows":queryresult}
+            else:
+                app.logger.warn('Empty data received. report status of scenarios.')
+        except Exception as getreportstatusexc:
+            servicesException("reportStatusScenarios_ICE",getreportstatusexc)
+        return jsonify(res)
+
+
+    #fetching the reports
+    @app.route('/reports/getReport_Nineteen68',methods=['POST'])
+    def getReport_Nineteen68():
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            app.logger.debug("Inside getReport_Nineteen68. Query: "+str(requestdata["query"]))
+            if not isemptyrequest(requestdata):
+                if(requestdata["query"] == 'projectsUnderDomain'):
+                    queryresult = list(n68session2.reports.find({"_id":ObjectId(requestdata["reportid"])},{"executionid":1,"executedtime":1,"report":1,"testscenarioid":1,"cycle":1}))
+                    res= {"rows":queryresult}
+                elif(requestdata["query"] == 'scenariodetails'):
+                    queryresult = list(n68session2.testscenarios.find({"_id":ObjectId(requestdata["scenarioid"]),"deleted":False},{"name":1,"projectid":1}))
+                    res= {"rows":queryresult}
+                elif(requestdata["query"] == 'cycledetails'):
+                    queryresult = list(n68session2.projects.aggregate([{"$match":{"_id":ObjectId(requestdata["projectid"]), "deleted":False}},{"$unwind":'$releases'},{"$unwind":'$releases.cycles'},{"$match":{"releases.cycles._id":ObjectId(requestdata["cycleid"])}},{"$project":{"name":1,"releases.name":1,"releases.cycles.name":1,"domain":1}}]))
+                    res= {"rows":queryresult}
+            else:
+                app.logger.warn('Empty data received. report.')
+        except Exception as getreportexc:
+            servicesException("getReport_Nineteen68",getreportexc)
+        return jsonify(res)
+
+
+    #update jira defect id in report data
+    @app.route('/reports/updateReportData',methods=['POST'])
+    def updateReportData():
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            app.logger.debug("Inside updateReportData.")
+            if not isemptyrequest(requestdata):
+                queryresult = n68session2.reports.find({"_id":ObjectId(requestdata["reportid"])},{"report":1})
+                report = queryresult[0]['report']
+                report_rows = report['rows']
+                row = None
+                for obj in report_rows:
+                    if obj['id']==int(requestdata['slno']):
+                        row = obj
+                    if "'" in obj['StepDescription']:
+                        obj['StepDescription'] = obj['StepDescription'].replace("'",'"')
+                if(row!=None):
+                    row.update({'jira_defect_id':str(requestdata['defectid'])})
+                    report['rows']=report_rows
+                    queryresult = n68session2.reports.update({"_id":ObjectId(requestdata["reportid"])},{"$set":{"report":report}})
+                    res={'rows':'Success'}
+        except Exception as updatereportdataexc:
+            app.logger.debug(updatereportdataexc)
+            servicesException("updateReportData",updatereportdataexc)
+        return jsonify(res)
+
+
+    @app.route('/reports/getWebocularData_ICE',methods=['POST'])
+    def getWebocularData_ICE():
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            if not isemptyrequest(requestdata):
+                app.logger.debug("Inside getWebocularData_ICE.")
+                if(requestdata["query"] == 'moduledata'):
+                    reports_data=list(webocularsession.reports.find({},{"_id":1,"modulename":1}))
+                    res={'rows':reports_data}
+                elif(requestdata["query"] == 'reportdata'):
+                    reports_data=list(webocularsession.reports.find({"_id":ObjectId(requestdata["id"])}))
+                    res={'rows':reports_data}
+                elif(requestdata["query"] == 'insertdata'):
+                    webocularsession.reports.insert_one(requestdata['data'])
+                    res={'rows':'success'}
+        except Exception as getweboculardataexec:
+            app.logger.debug(getweboculardataexec)
+            servicesException("getWebocularData_ICE",getweboculardataexec)
+        return jsonify(res)
+
+
+# END OF REPORTS
