b52c1d58e2c902641c4bea378db539337e196ac0
Veeresh Koti <veeresh.koti@slkgroup.com>
2020-08-04 10:02:13 +0530

    #3662 : changes in naming newly added objects in case of duplicates

--- src/routes/designtestcaseservice.py
+++ src/routes/designtestcaseservice.py
@@ -194,9 +194,25 @@ def LoadServices(app, redissession, dbsession):
                                 cid = custnames[cname]["_id"]
                             else:
                                 cid = ObjectId()
-                                so["custname"] = cname+str(cid)
-                                custnames[so["custname"]] = {"_id":cid,"xpath":so["objectName"],"url":so['url'] if 'url' in so else ""}
-                                missingCustname[cid] = so
+                                try:
+                                    s_cname = cname.split('_')
+                                    ind = int(s_cname.pop())
+                                    n_cname = '_'.join(s_cname)
+                                except ValueError:
+                                    ind = 0
+                                    n_cname = cname
+                                while True:
+                                    if n_cname+'_'+str(ind+1) not in custnames:
+                                        so["custname"] = n_cname+'_'+str(ind+1)
+                                        break
+                                    else:
+                                        if ('objectName' in so) and ('xpath' in custnames[n_cname+'_'+str(ind+1)]) and (so["objectName"] == custnames[n_cname+'_'+str(ind+1)]['xpath']):
+                                            cid = custnames[n_cname+'_'+str(ind+1)]["_id"]
+                                            break
+                                    ind += 1
+                                if so["custname"] not in custnames:
+                                    custnames[so["custname"]] = {"_id":cid,"xpath":so["objectName"],"url":so['url'] if 'url' in so else ""}
+                                    missingCustname[cid] = so
                         elif (cname not in custnames) and (cname not in defcn):
                             cid = ObjectId()
                             custnames[cname] = {"_id":cid,"xpath":so["objectName"],"url":so['url'] if 'url' in so else ""}
