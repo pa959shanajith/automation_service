3668bcca91f01ddb5d8e37a9405e1227e27bd654
Anvesh Sharma <anvesh.sharma@slkgroup.com>
2021-06-30 14:56:14 +0530

    #25578: Execution screen scenario content testcase list popup is empty

--- src/routes/executionservice.py
+++ src/routes/executionservice.py
@@ -195,15 +195,15 @@ def LoadServices(app, redissession, dbsession):
                             if 'dtparam' in requestdata:
                                 dtp = requestdata['dtparam']
                                 if len(dtp) > 0 and dtp[0] not in dtnames: dtnames.append(dtp[0])
-                                if len(dtnames) > 0:
-                                    dts = []
-                                    dts_to_fetch = [i for i in dtnames if i not in dts_data]
-                                    dtdet = dbsession.datatables.find({"name": {'$in': dts_to_fetch}})
-                                    for dt in dtdet: dts_data[dt['name']] = dt['datatable']
-                                    for dt in dtnames: 
-                                        if dt in dts_data:
-                                            dts.append({dt: dts_data[dt]})
-                                    tc['datatables'] = dts
+                            if len(dtnames) > 0:
+                                dts = []
+                                dts_to_fetch = [i for i in dtnames if i not in dts_data]
+                                dtdet = dbsession.datatables.find({"name": {'$in': dts_to_fetch}})
+                                for dt in dtdet: dts_data[dt['name']] = dt['datatable']
+                                for dt in dtnames: 
+                                    if dt in dts_data:
+                                        dts.append({dt: dts_data[dt]})
+                                tc['datatables'] = dts
                             testcase.append(tc)
                         res["rows"] = testcase
                     if 'userid' in requestdata:    # Update the Counter
