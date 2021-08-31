86de7cd18fcf807ee39608a963beed19b3714c81
divyansh.singh <divyansh.singh@slkgroup.com>
2021-08-31 18:02:44 +0530

    #28185: Task - Added bulk writes and aggregations for notificationservices

--- src/routes/notificationservice.py
+++ src/routes/notificationservice.py
@@ -2,7 +2,7 @@
 # BEGIN OF DEFAULT METHODS AND IMPORTS       -----------DO NOT EDIT
 ################################################################################
 #----------DEFAULT METHODS AND IMPORTS------------DO NOT EDIT-------------------
-from pymongo.operations import InsertOne, UpdateOne, DeleteOne, FindOne
+from pymongo.operations import InsertOne, UpdateOne, DeleteOne
 from utils import *
 from Crypto.Cipher import AES
 import base64
@@ -28,13 +28,49 @@ def LoadServices(app, redissession, dbsession):
             requestdata = json.loads(request.data)
             if not isemptyrequest(requestdata):
                 result = []
-                groupids = [ObjectId(id) for id in requestdata.groupids]
-                groupnames = requestdata.groupnames
+                groupids = [ObjectId(id) for id in requestdata['groupids']]
+                groupnames = requestdata['groupnames']
                 if len(groupids) == 0 and len(groupnames) == 0:
-                    result.extend(list(dbsession.notificationgroups.find({},{"groupname":1,"internalusers":1,"otherusers":1})))
+                    result.extend(list(dbsession.notificationgroups.find({},{"groupname":1,"_id":1})))
                 else:
-                    result.extend(list(dbsession.notificationgroups.find({'_id':{'$in':groupids}},{"groupname":1,"internalusers":1,"otherusers":1})))
-                    result.extend(list(dbsession.notificationgroups.find({'groupname':{'$in':groupnames}},{"groupname":1,"internalusers":1,"otherusers":1})))
+                    user_lookup = {
+                                    '$lookup':
+                                        {
+                                            'from': "users",
+                                            'localField': "internalusers",
+                                            'foreignField': "_id",
+                                            'as': "internal_user_info"
+                                        },         
+                                }
+                    projection = {
+                                    "$project": {
+                                        "_id": 1,
+                                        "otherusers": 1,
+                                        "internal_user_info._id": 1,
+                                        "internal_user_info.email": 1,
+                                        "internal_user_info.name": 1,
+                                    }
+                                }
+                                    
+                    id_aggregator = [
+                                        { 
+                                            '$match':{'_id':{'$in':groupids}}
+                                        },
+                                        user_lookup,
+                                        projection
+                                    ] 
+                    name_aggregator = [
+                                        { 
+                                            '$match':{"groupname":{'$in':groupnames}}
+                                        },
+                                        user_lookup,
+                                        projection
+                                    ]
+                    result_ids = list(dbsession.notificationgroups.aggregate(id_aggregator))
+                    result_names = list(dbsession.notificationgroups.aggregate(name_aggregator))
+                    result.extend(result_ids)
+                    result.extend(result_names)
+
                 res['rows'] = result
                 del res['err']
             else:
@@ -49,41 +85,39 @@ def LoadServices(app, redissession, dbsession):
     def updateNotificationGroups():
         app.logger.debug("Inside updateNotificationGroups")
         res={'rows':'fail','err':''}
-
         try:
-            requestdata=json.loads(request.data)
             requestdata = json.loads(request.data)
             if not isemptyrequest(requestdata):
-                if requestdata.action.lower() == "create":
-                    groupdata = requestdata.groupdata
+                groupdata = requestdata['groupdata']
+                if requestdata['action'].lower() == "create":
                     query = [InsertOne(
                                         {
+                                        'groupname': groupdata[id]['groupname'],
                                         'createdon': datetime.now(),
-                                        'createdbyrole': ObjectId(requestdata.modifiedbyrole),
-                                        'createdby': ObjectId(requestdata.modifiedby),
-                                        'internalusers': groupdata[id]['internalusers'],
+                                        'createdbyrole': ObjectId(requestdata['modifiedbyrole']),
+                                        'createdby': ObjectId(requestdata['modifiedby']),
+                                        'internalusers': [ObjectId(iuid) for iuid in groupdata[id]['internalusers']],
                                         'otherusers': groupdata[id]['otherusers']
                                         }
                                     ) for id in groupdata ]
                    
                     dbsession.notificationgroups.bulk_write(query)
                 
-                elif requestdata.action.lower() == "update":
-                    groupdata = requestdata.groupdata
+                elif requestdata['action'].lower() == "update":
                     query = [UpdateOne(
                                         {'_id' : ObjectId(id)}, 
                                         {'$set' : {
+                                            'groupname': groupdata[id]['groupname'],
                                             'modifiedon': datetime.now(),
-                                            'modifiedbyrole': ObjectId(requestdata.modifiedbyrole),
-                                            'modifiedby': ObjectId(requestdata.modifiedby),
-                                            'internalusers': groupdata[id]['internalusers'],
+                                            'modifiedbyrole': ObjectId(requestdata['modifiedbyrole']),
+                                            'modifiedby': ObjectId(requestdata['modifiedby']),
+                                            'internalusers': [ObjectId(iuid) for iuid in groupdata[id]['internalusers']],
                                             'otherusers': groupdata[id]['otherusers']}
                                             }
                                     ) for id in groupdata ]
 
                     dbsession.notificationgroups.bulk_write(query);                
-                elif requestdata.action.lower() == "delete":
-                    groupdata = requestdata.groupdata
+                elif requestdata['action'].lower() == "delete":
                     query = [DeleteOne({'_id' : ObjectId(id)}) for id in groupdata]
                     dbsession.notificationgroups.bulk_write(query);                
                
