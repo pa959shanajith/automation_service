c9be6fc47484330399012f9e67c0d49a0b40b3bb
Monisha Tarkar <monisha.tarkar@slkgroup.com>
2022-04-25 12:10:39 +0000

    added iTDM keyword in utils.py

--- src/utils.py
+++ src/utils.py
@@ -26,7 +26,7 @@ dasport = "1990"
 
 ui_plugins = {"alm":"Integration","apg":"APG","dashboard":"Dashboard",
     "mindmap":"Mindmap","neurongraphs":"Neuron Graphs","performancetesting":"Performance Testing",
-    "reports":"Reports","utility":"Utility","weboccular":"Webocular","seleniumtoavo":"Selenium To Avo", "avodiscover": "Avo Discover"}
+    "reports":"Reports","utility":"Utility","weboccular":"Webocular","seleniumtoavo":"Selenium To Avo", "avodiscover": "Avo Discover", "iTDM" : "iTDM"}
 
 projecttype_names={}
 
