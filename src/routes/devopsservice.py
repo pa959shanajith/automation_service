58f25fe61db9fb18c6d7d28f77938901fe249e43
Sushanth Gupta <sushanth.gupta1@slksoft.com>
2022-07-26 12:51:16 +0530

    Added UI API's and updated current API's to work for versions

--- src/routes/devopsservice.py
+++ src/routes/devopsservice.py
@@ -12,10 +12,27 @@ def LoadServices(app, redissession, dbsession):
         res={'rows':'fail'}
         try:
             requestdata=json.loads(request.data)
-            requestdata["token"] = "thirdkey"
+            requestdata["token"] = requestdata["executionRequest"]['configurekey']
             # print(Key)
             # To Do delete query key from requestdata.
-            dbsession.configurekeys.insert_one(requestdata)
+
+            # check whether key is already present
+            keyAlreadyExist = list(dbsession.configurekeys.find({'token': requestdata["token"]}))
+
+            # case-1 key not present
+            if(len(keyAlreadyExist) == 0):
+                newRequestData = {item: requestdata[item] for item in requestdata if item not in {'executionRequest'}}
+                requestdata["executionRequest"]['edited'] = 0
+                newRequestData["executionRequestList"] = [{"executionRequest": requestdata["executionRequest"]}]
+                dbsession.configurekeys.insert_one(newRequestData)
+
+            else: #key is present
+                newList = {}
+                newList["executionRequestList"] = keyAlreadyExist[0]["executionRequestList"]
+                requestdata["executionRequest"]['edited'] = keyAlreadyExist[0]["executionRequestList"][-1]['executionRequest']['edited'] + 1
+                newList["executionRequestList"].append({"executionRequest": requestdata["executionRequest"]})
+                dbsession.configurekeys.update({"_id":ObjectId(keyAlreadyExist[0]['_id'])},{'$set':{"executionRequestList":newList["executionRequestList"]}})
+           
             res['rows'] = 'success'
             # if not isemptyrequest(requestdata):
             #     print("I am inside")
@@ -34,10 +51,14 @@ def LoadServices(app, redissession, dbsession):
             # print(Key)
             # To Do delete query key from requestdata.
             queryresult = list(dbsession.configurekeys.find({"token": requestdata['key']}))
-            print(queryresult[0]['executionRequest']['testsuiteIds'])
+            # print(queryresult[0]['executionRequest']['testsuiteIds'])
+            print(queryresult[0]['executionRequestList'][-1]['executionRequest']['testsuiteIds'])
+
             res['rows'] = {
-                "testSuiteInfo" : queryresult[0]['executionRequest']['testsuiteIds'] ,
-                "avogridid" : queryresult[0]['executionRequest']['avogridid']
+                "testSuiteInfo" : queryresult[0]['executionRequestList'][-1]['executionRequest']['testsuiteIds'],
+                "edited" : queryresult[0]['executionRequestList'][-1]['executionRequest']['edited'],
+                "avoagentList": queryresult[0]['executionRequestList'][-1]['executionRequest']['avoagents']
+                # "avogridid" : queryresult[0]['executionRequest']['avogridid']
             }
             # if not isemptyrequest(requestdata):
             #     print("I am inside")
@@ -75,8 +96,16 @@ def LoadServices(app, redissession, dbsession):
             # requestdata["token"] = "firstkey"
             # print(Key)
             # To Do delete query key from requestdata.
-            dbsession.avoagents.insert_one(requestdata)
-            res['rows'] = 'success'
+
+            agentPresent = list(dbsession.avoagents.find({"Hostname": requestdata["Hostname"]}))
+            if(len(agentPresent) > 0):
+                requestdata['status'] = agentPresent[0]['status']
+                requestdata['createdon'] = agentPresent[0]['createdon']
+                dbsession.avoagents.update({"_id":ObjectId(agentPresent[0]['_id'])},{'$set':{"recentCall":requestdata['recentCall']}})
+            else:
+                dbsession.avoagents.insert_one(requestdata)
+
+            res['rows'] = requestdata
             # if not isemptyrequest(requestdata):
             #     print("I am inside")
 
@@ -94,11 +123,20 @@ def LoadServices(app, redissession, dbsession):
             # requestdata["token"] = "firstkey"
             # print(Key)
             # To Do delete query key from requestdata.
+            # fetching keys which contains given avo agent
             data = list(dbsession.configurekeys.find({"executionRequest.avoagents" : {"$in": [requestdata['avoagents']]}},{'_id': 0,'token': 1}))
             keysList = []
             for dic in data:
                 for val in dic.values():
                     keysList.append(val)
+            
+            # fetching keys which contains no agents => agent list is empty for these keys.
+            if len(keysList) == 0:
+                data = list(dbsession.configurekeys.find({"executionRequest.avoagents" : []},{'_id': 0,'token': 1}))
+                keysList = []
+                for dic in data:
+                    for val in dic.values():
+                        keysList.append(val)
 
 
             res['rows'] = keysList
@@ -119,6 +157,16 @@ def LoadServices(app, redissession, dbsession):
             requestdata=json.loads(request.data)
             testSuiteId = requestdata['testSuiteId']
             executionData = list(dbsession.configurekeys.find({"token": requestdata['key']}))
+            correctexecutionRequest = ''
+            for executionRequest in executionData[0]['executionRequestList']:
+                # print(executionRequest['executionRequest']['edited'])
+                if executionRequest['executionRequest']['edited'] == requestdata['edited']:
+                    # print('what')
+                    correctexecutionRequest = executionRequest
+                    break
+
+            executionData[0].pop('executionRequestList')
+            executionData[0]['executionRequest'] = correctexecutionRequest['executionRequest']
             print(executionData[0]['executionRequest']['testsuiteIds'].index(testSuiteId))
             index = executionData[0]['executionRequest']['testsuiteIds'].index(testSuiteId)
             executionData[0]['executionRequest']['executionIds'] = [executionData[0]['executionRequest']['executionIds'][index]]
@@ -152,10 +200,12 @@ def LoadServices(app, redissession, dbsession):
             for moduleDetail in requestdata:
                 scenarioids=[]
                 mindmapdata=dbsession.mindmaps.find_one({"_id":ObjectId(moduleDetail['_id'])},{"testscenarios":1})
+                testsuitesdata=dbsession.testsuites.find_one({"mindmapid":ObjectId(moduleDetail['_id'])},{"batchname":1})
                 processedData.append({
                     'moduleid' : moduleDetail['_id'],
                     "name" : moduleDetail['name'],
-                    'scenarios' : []
+                    'scenarios' : [],
+                    'batchname': testsuitesdata['batchname']
                 })
                 if "testscenarios" in mindmapdata:
                     for ts in mindmapdata["testscenarios"]:
@@ -168,6 +218,61 @@ def LoadServices(app, redissession, dbsession):
 
             print(processedData)
             res['rows'] = processedData
+
+        except Exception as e:
+            print(e)
+            return e
+        return jsonify(res)
+
+
+    @app.route('/devops/getConfigureList',methods=['POST'])
+    def getConfigureList():
+        app.logger.debug("Inside getConfigureList")
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            # print(Key)
+            # To Do delete query key from requestdata.
+            queryresult = list(dbsession.configurekeys.find({'executionRequestList': { '$elemMatch' : {'executionRequest.invokinguser': '60c071d5a7ea21cf5185ef6e'}}}))
+            responseData = []
+            for elements in queryresult:
+                updatedExecutionReq = elements['executionRequestList'][-1]['executionRequest']
+                responseData.append({
+                    'configurename': updatedExecutionReq['configurename'],
+                    'configurekey': updatedExecutionReq['configurekey'],
+                    'project': updatedExecutionReq['suitedetails'][0]['projectname'],
+                    'release': updatedExecutionReq['suitedetails'][0]['releaseid']
+                })
+            print(responseData)
+            
+            res['rows'] = responseData
+
+            # if not isemptyrequest(requestdata):
+            #     print("I am inside")
+
+        except Exception as e:
+            print(e)
+            return e
+        return jsonify(res)
+
+    @app.route('/devops/getAvoAgentAndAvoGridList',methods=['POST'])
+    def getAvoAgentAndAvoGridList():
+        app.logger.debug("Inside getAvoAgentAndAvoGridList")
+        res={'rows':'fail'}
+        try:
+            requestdata=json.loads(request.data)
+            # print(Key)
+            # To Do delete query key from requestdata.
+            queryresultavoagent = list(dbsession.avoagents.find({}))
+            queryresultavogrid = list(dbsession.avogrids.find({}))
+
+            res['rows'] = {
+                'avoagents': queryresultavoagent,
+                'avogrids': queryresultavogrid
+            }
+
+            # if not isemptyrequest(requestdata):
+            #     print("I am inside")
 
         except Exception as e:
             print(e)
