800d46dac86f74d8bb022c450244b258cf3cd25f
Sathwik P Koushik <sathwik.p@avoautomation.com>
2023-06-08 16:09:13 +0530

    Linux Build Files update

--- build/Linux/custom_python3.7.6.sh
+++ build/Linux/custom_python3.7.6.sh
@@ -2,14 +2,14 @@
 sudo yum install gcc openssl-devel bzip2-devel libffi-devel sqlite-devel;
 wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz
 tar xzf Python-3.7.6.tgz;
-DIR=~/Build/python3.7.6
+DIR=~/Build/Python3.7.6
 if [ -d "$DIR" ]; then
-    echo "python3.7.6 is already present"
+    echo "Python3.7.6 is already present"
 else
-    echo "python3.7.6 NOT found."
-    mkdir -p ~/Build/python3.7.6 
+    echo "Python3.7.6 NOT found."
+    mkdir -p ~/Build/Python3.7.6 
     cd ~/Build/Python-3.7.6
-    export pypath=~/Build/python3.7.6 
+    export pypath=~/Build/Python3.7.6 
     echo $pypath
     ./configure --enable-optimizations --prefix=$pypath --enable-shared LDFLAGS="-Wl,-rpath,'\$\$ORIGIN/../lib'"
     make altinstall
