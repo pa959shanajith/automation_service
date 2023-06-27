e1685abd9ceffdd4fd8cbe6a465127b27e6d3db1
Sathwik P <sathwik.p@slkgroup.com>
2023-06-27 07:03:41 +0000

    Updated custom_python3.7.6.sh

--- build/Linux/custom_python3.7.6.sh
+++ build/Linux/custom_python3.7.6.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 sudo yum install gcc openssl-devel bzip2-devel libffi-devel sqlite-devel;
+rm -rf Python-3.7.6.tgz Python-3.7.6
 wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz
 tar xzf Python-3.7.6.tgz;
 #mkdir -p ~/Build/Python3.7.6
-DIR=~/Build/Python3.7.6
-if [ -d "$DIR" ]; then
-    echo "Python3.7.6 is already present"
-else
-    echo "Python3.7.6 NOT found."
+#DIR=~/Build/Python3.7.6
+#if [ -d "$DIR" ]; then
+#    echo "Python3.7.6 is already present"
+#else
+    #echo "Python3.7.6 NOT found."
     #mkdir -p ~/Build/Python3.7.6 
-    cd Python-3.7.6
-    export pypath=~/Build/Python3.7.6 
-    echo $pypath
-    ./configure --enable-optimizations --prefix=$pypath --enable-shared LDFLAGS="-Wl,-rpath,'\$\$ORIGIN/../lib'"
-    make altinstall
-fi
+    #cd Python-3.7.6
+    #export pypath=~/Build/Python3.7.6 
+    #echo $pypath
+    #./configure --enable-optimizations --prefix=$pypath --enable-shared LDFLAGS="-Wl,-rpath,'\$\$ORIGIN/../lib'"
+    #make altinstall
+#fi
+
+rm -rf custom_python3.7.6
+mkdir custom_python3.7.6 
+export pypath=$(pwd)/custom_python3.7.6 
+cd Python-3.7.6
+echo $pypath
+./configure --enable-optimizations --prefix=$pypath --enable-shared LDFLAGS="-Wl,-rpath,'\$\$ORIGIN/../lib'"
+make altinstall
