29647d067eb14aee038e77349464b5c5bb5f49b5
Sathwik P <sathwik.p@slkgroup.com>
2023-08-21 11:16:51 +0000

    Updated custom_python3.7.6.sh

--- build/Linux/custom_python3.7.6.sh
+++ build/Linux/custom_python3.7.6.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo yum install gcc openssl-devel bzip2-devel libffi-devel sqlite-devel;
 #rm -rf Python-3.7.6.tgz Python-3.7.6
+echo 11111
 wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz;
 tar xzf Python-3.7.6.tgz;
 #mkdir -p ~/Build/Python3.7.6
@@ -18,9 +19,14 @@ tar xzf Python-3.7.6.tgz;
 #fi
 
 #rm -rf custom_python3.7.6
+echo 222222
 mkdir custom_python3.7.6;
+echo 333333
 export pypath=$(pwd)/custom_python3.7.6;
+echo 44444
 cd Python-3.7.6;
 echo $pypath;
 ./configure --enable-optimizations --prefix=$pypath --enable-shared LDFLAGS="-Wl,-rpath,'\$\$ORIGIN/../lib'";
+echo 555555
 make altinstall;
+echo 66666
