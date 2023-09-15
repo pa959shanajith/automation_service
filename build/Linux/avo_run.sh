8352cc2bb5f9b9d4f094f8693fe9506162e2f8ba
Sathwik P <sathwik.p@slkgroup.com>
2023-09-15 16:50:45 +0000

    Updated avo_run.sh

--- build/Linux/avo_run.sh
+++ build/Linux/avo_run.sh
@@ -5,8 +5,8 @@ sed -i 's|DB_NAME=avoassure|DB_NAME='"$DB_NAME"'|g' /home/webapp/.env;
 ldconfig /usr/local/lib;
 /usr/bin/redis-server /etc/redis.conf --daemonize yes --supervised systemd >> /home/status.txt 2>&1;
 /usr/sbin/nginx;
-nohup /home/DAS/run.sh >> /home/status.txt 2>&1
-#nohup /home/webapp/run.sh >> /home/status.txt 2>&1
+nohup /home/DAS/run.sh >> /home/status.txt 2>&1 &
+nohup /home/webapp/run.sh >> /home/status.txt 2>&1
 echo "Avo Assure server started"
 
 
