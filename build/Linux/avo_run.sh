467fdb7a0ab37c887dd99904d367fb29b7b0915e
Sathwik P <sathwik.p@slkgroup.com>
2023-09-15 10:49:19 +0000

    Updated avo_run.sh

--- build/Linux/avo_run.sh
+++ build/Linux/avo_run.sh
@@ -2,7 +2,7 @@
 sed -i 's|^DB_IP=127.0.0.1|DB_IP='"$DB_IP"'|1' /home/webapp/.env;
 sed -i 's|DB_PORT=27017|DB_PORT='"$DB_PORT"'|g' /home/webapp/.env;
 sed -i 's|DB_NAME=avoassure|DB_NAME='"$DB_NAME"'|g' /home/webapp/.env;
-sudo ldconfig /usr/local/lib;
+ldconfig /usr/local/lib;
 /usr/bin/redis-server /etc/redis.conf --daemonize yes --supervised systemd >> /home/status.txt 2>&1;
 /usr/sbin/nginx;
 nohup /home/DAS/run.sh >> /home/status.txt 2>&1 &
