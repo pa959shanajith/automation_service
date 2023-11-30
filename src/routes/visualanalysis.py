0ba7d796ee44ec64968b76f0994df14a6526227b
Shaurya Suman <shaurya.suman@avoautomation.com>
2023-12-01 00:35:11 +0530

    Added defect analysis and teststep execution status APIs

--- src/routes/visualanalysis.py
+++ src/routes/visualanalysis.py
@@ -14,11 +14,12 @@ def LoadServices(app, redissession, client,getClientName):
     #########################################################################
 
     # Function for request validation
-    def validate_request(data, require_projectid=False, require_userid=False, require_profileid=False, require_execListid=False):
+    def validate_request(data, require_projectid=False, require_userid=False, require_profileid=False, require_execListid=False, require_executionid=False):
         projectid = data.get("projectid")
         userid = data.get("userid")
         profileid = data.get("profileid")
         execListID = data.get("execlistid")
+        executionid = data.get("executionid")
         start_time = data.get("start_time")
         end_time = data.get("end_time")
 
@@ -47,12 +48,17 @@ def LoadServices(app, redissession, client,getClientName):
         # Check whether "profileid" is present or not
         if require_execListid and not execListID:
             return {"error": "execListID is required"}
+        
+        # Check whether "executionid" is present or not
+        if require_executionid and not executionid:
+            return {"error": "executionid is required"}
 
         return {
             "projectid": projectid,
             "userid": userid,
             "profileid": profileid,
             "execListID": execListID,
+            "executionid": executionid,
             "start_time": start_time,
             "end_time": end_time,
         }
@@ -170,7 +176,7 @@ def LoadServices(app, redissession, client,getClientName):
 
 
     # Function return module level execution status within a specified time range (for an execution)
-    def fetch_modulelevel_execution_status(requestdata, executionlistid, starttime, endtime):
+    def fetch_modulelevel_execution_status(requestdata, executionlistid):
         try:
             clientName = getClientName(requestdata) 
             dbsession = client[clientName]
@@ -178,7 +184,6 @@ def LoadServices(app, redissession, client,getClientName):
             return e
 
         try:
-            # start_datetime, end_datetime = date_conversion(starttime, endtime)
             reports = list(dbsession.executions.aggregate([{"$match":{"executionListId":executionlistid}},
                                                         {"$project":{"parent":1,"status":1,"starttime":1}},
                                                         {'$lookup':{
@@ -219,6 +224,143 @@ def LoadServices(app, redissession, client,getClientName):
             return result_list
         except Exception as e:
             return e
+
+
+    # Function to fetch test step pass and fail count for a specific module
+    def fetch_teststep_execution_status(requestdata, executionid):
+        try:
+            clientName = getClientName(requestdata) 
+            dbsession = client[clientName]
+        except Exception as e:
+            return e
+        
+        try:
+            pipeline = [
+                    { "$match": { "executionid": executionid } },
+                    { "$group": {
+                        "_id": {  
+                        "executionid": "$executionid",
+                        "reportid": "$reportid",
+                        "scenarioid": "$scenarioid", 
+                        "status": "$status"
+                        },
+                        "count": { "$sum": 1 }
+                    }},
+                    { "$group": {
+                        "_id": {
+                        "executionid": "$_id.executionid",  
+                        "reportid": "$_id.reportid",
+                        "scenarioid": "$_id.scenarioid"
+                        },
+                        "statusCount": {
+                        "$push": { 
+                            "status": "$_id.status",
+                            "count": "$count"  
+                        }
+                        }
+                    }}
+                ]
+            result = dbsession.reportitems_copy.aggregate(pipeline)
+
+            transformed_data = []
+            for entry in result:
+                report_id = str(entry['_id']['reportid'])
+                scenario_id = entry['_id']['scenarioid']
+
+                # Fetch TCgroup_name from testscenario collection
+                tc_group_name = dbsession.testscenarios.find_one({'_id': ObjectId(scenario_id)})['name']
+
+                # Extract Pass and Fail counts
+                pass_count = next((status['count'] for status in entry['statusCount'] if status.get('status') == 'Pass'), 0)
+                fail_count = next((status['count'] for status in entry['statusCount'] if status.get('status') == 'Fail'), 0)
+
+                # Create the desired format
+                result_entry = {
+                    "reportid": report_id,
+                    "scenarioid": scenario_id,
+                    "TCgroup_name": tc_group_name,
+                    "TCStatus": {
+                        "Pass": pass_count,
+                        "Fail": fail_count
+                    }
+                }
+                transformed_data.append(result_entry)
+
+            return transformed_data
+        except Exception as e:
+            return e
+
+
+    # Function to fetch defect analysis data
+    def fetch_defect_data(requestdata, projectid, userid, starttime, endtime):
+        try:
+            clientName = getClientName(requestdata) 
+            dbsession = client[clientName]
+        except Exception as e:
+            return e
+
+        try:
+            start_datetime, end_datetime = date_conversion(starttime, endtime)
+            result = dbsession.configurekeys.find({"executionData.batchInfo.projectId": projectid,
+                                                            "session.userid": userid},
+                                                            {"_id":0,"token":1, 
+                                                            "executionData.configurename":1})
+            # Dictionary to store the transformed data
+            transformed_data = []
+
+            # Iterate over each profileid
+            for key in result:
+                profileid = key['token']
+                profilename = key['executionData']['configurename']
+                temp = dbsession.executions.aggregate([{
+                                                        "$match": {
+                                                            "configurekey": profileid,
+                                                            "starttime": {"$gte": start_datetime, "$lte": end_datetime}
+                                                            }
+                                                        },
+                                                    {
+                                                        "$group": {
+                                                            "_id":"$executionListId",
+                                                            "executionids": {"$push": {"_id": "$_id"}},
+                                                            }
+                                                        },
+                                                    ])
+                
+                defect_details = []
+                count = 0
+                for value in temp:
+                    count += 1
+                    executionlist_ID = value['_id']
+                    execlist_name = "Execution" + str(count)
+                    executionid_list = [str(execution['_id']) for execution in value.get('executionids', [])]
+
+                    failcount_result = dbsession.reportitems_copy.aggregate([{
+                                                                                "$match": {
+                                                                                "executionid": {"$in": executionid_list},
+                                                                                "Keyword": {"$ne": "TestCase Name"}, 
+                                                                                "status": "Fail"}
+                                                                            },
+                                                                            {"$group": {"_id": None, "count": {"$sum": 1}}}
+                                                                            ])
+                    
+                    defect_details.append({
+                        "execlistid": executionlist_ID,
+                        "execlist_name": execlist_name,
+                        "fail_count": failcount_result['count']
+                    })
+
+                # Update the transformed data dictionary
+                transformed_data.append({
+                    "profileid": profileid,
+                    "profilename": profilename,
+                    "defect_details": defect_details
+                })    
+
+            return transformed_data
+            
+        except Exception as e:
+            return e
+
 
 
     #########################################################################
@@ -276,7 +418,7 @@ def LoadServices(app, redissession, client,getClientName):
             start_time = request_data["start_time"]
             end_time = request_data["end_time"]
 
-            result = fetch_modulelevel_execution_status(requestdata, execListID, start_time, end_time)
+            result = fetch_modulelevel_execution_status(requestdata, execListID)
             print(json.dumps({"data": result, "start_time": start_time, "end_time": end_time, "status": 200}, indent=2))
 
             # Convert the date string to the desired format
@@ -285,6 +427,68 @@ def LoadServices(app, redissession, client,getClientName):
 
             return jsonify({"data": result, "start_time": start_time, "end_time": end_time, "status": 200})
 
+        # Handle any exceptions with a 500 Internal Server Error response
+        except Exception as e:
+            return jsonify({"data": {"message": str(e)}, "status": 500})
+        
+    
+    # POST API to fetch test steps level executions status
+    @app.route("/teststepLevel_ExecutionStatus", methods=["POST"])
+    def teststep_execution_analysis():
+        app.logger.debug("Inside TestStep_Level_Execution_Status")
+        try:
+            requestdata = request.get_json()
+            request_data = validate_request(requestdata, require_executionid=True)
+            if "error" in request_data:
+                return jsonify({"data": request_data["error"], "status": 400})
+            
+            executionid = request_data["executionid"]
+            start_time = request_data["start_time"]
+            end_time = request_data["end_time"]
+
+            result = fetch_teststep_execution_status(requestdata, executionid)
+            print(json.dumps({"data": result, "start_time": start_time, "end_time": end_time, "status": 200}, indent=2))
+
+            # Convert the date string to the desired format
+            start_time = datetime.strptime(start_time, "%Y-%m-%d").strftime("%d/%m/%Y")
+            end_time = datetime.strptime(end_time, "%Y-%m-%d").strftime("%d/%m/%Y")
+        
+            return jsonify({"data": result, "start_time": start_time, "end_time": end_time, "status": 200})
+
+        # Handle any exceptions with a 500 Internal Server Error response
+        except Exception as e:
+            return jsonify({"data": {"message": str(e)}, "status": 500})
+
+
+    #########################################################################
+    ########################## DEFECT ANALYSIS API ##########################
+    #########################################################################
+    
+    # POST API to fetch defect executions
+    @app.route("/defect_analysis", methods=["POST"])
+    def api_defect_execution_analysis():
+        app.logger.debug("Inside Defect Analysis")
+        try:
+            requestdata = request.get_json()
+            request_data = validate_request(requestdata, require_projectid=True, require_userid=True)
+
+            if "error" in request_data:
+                return jsonify({"data": request_data["error"], "status": 400})
+            
+            projectid = request_data["projectid"]
+            userid = request_data["userid"]
+            start_time = request_data["start_time"]
+            end_time = request_data["end_time"]
+
+            result = fetch_defect_data(requestdata, projectid, userid, start_time, end_time)
+            print(json.dumps({"data": result, "start_time": start_time, "end_time": end_time, "status": 200}, indent=2))
+
+            # Convert the date string to the desired format
+            start_time = datetime.strptime(start_time, "%Y-%m-%d").strftime("%d/%m/%Y")
+            end_time = datetime.strptime(end_time, "%Y-%m-%d").strftime("%d/%m/%Y")
+        
+            return jsonify({"data": result, "start_time": start_time, "end_time": end_time, "status": 200})
+        
         # Handle any exceptions with a 500 Internal Server Error response
         except Exception as e:
             return jsonify({"data": {"message": str(e)}, "status": 500})
